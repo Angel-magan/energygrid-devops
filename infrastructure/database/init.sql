555c47245a421fd23faaf62eb231839e153357a0
Angel Magana <maganaangel342@gmail.com>
2026-05-08 11:05:10 -0600

    Logs estructurados y Microservicios funcionando ingresando datos a BD desde el simulador

--- infrastructure/database/init.sql
+++ infrastructure/database/init.sql
@@ -1,3 +1,8 @@
--- Archivo de inicialización para EnergyGrid
--- Por ahora vacío, solo para asegurar que la DB levante correctamente.
-SELECT 1;
+CREATE TABLE IF NOT EXISTS telemetry (
+    id SERIAL PRIMARY KEY,
+    district_id VARCHAR(50) NOT NULL,
+    substation_id VARCHAR(50) NOT NULL,
+    consumption_kw DECIMAL(10, 2) NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
